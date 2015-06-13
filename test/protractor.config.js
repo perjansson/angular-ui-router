926dd83a95a8497f83dd63be3d9d3deb5cbfa395
Per Jansson <per.r.jansson@gmail.com>
2015-06-13 15:44:57 +0200

    Small fixes

--- test/protractor.config.js
+++ test/protractor.config.js
@@ -1,5 +1,5 @@
 exports.config = {
-	seleniumServerJar: '../node_modules/webdriver-manager/selenium/selenium-server-standalone-2.46.0.jar',
+	seleniumServerJar: '../node_modules/protractor/selenium/selenium-server-standalone-2.45.0.jar',
 	/*seleniumAddress: 'http://localhost:4444/wd/hub',*/
 	specs: ['todo-spec.js'],
 	seleniumPort: 4444,
